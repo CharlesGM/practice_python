f80f101c07b320c5918b4e75f779327bd284cf78
g <g@g>
2021-11-04 19:03:29 +0300

    first commit

---
+++ character_input.py
@@ -0,0 +1,5 @@
+name = input("Please enter your name: ")
+age = int(input("Please enter your age: "))
+future_age = str(int(2021) + int(99) - age)
+
+print("Dear " + name + ", you will turn 100 years in " + future_age)
